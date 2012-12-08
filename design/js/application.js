7bd15e29f45db3f4ba2294e8640cae8a7b986f8d
gamebak <alexandru.cobuz@gmail.com>
2012-12-08 20:01:02 +0200

    ajx

--- design/js/application.js
+++ design/js/application.js
@@ -1,35 +1,54 @@
-function whichBrs()
-{
-	var agt=navigator.userAgent.toLowerCase();
-	if (agt.indexOf("opera") != -1) return 'Opera';
-	if (agt.indexOf("staroffice") != -1) return 'Star Office';
-	if (agt.indexOf("webtv") != -1) return 'WebTV';
-	if (agt.indexOf("beonex") != -1) return 'Beonex';
-	if (agt.indexOf("chimera") != -1) return 'Chimera';
-	if (agt.indexOf("netpositive") != -1) return 'NetPositive';
-	if (agt.indexOf("phoenix") != -1) return 'Phoenix';
-	if (agt.indexOf("firefox") != -1) return 'Firefox';
-	if (agt.indexOf("safari") != -1) return 'Safari';
-	if (agt.indexOf("skipstone") != -1) return 'SkipStone';
-	if (agt.indexOf("msie") != -1) return 'Internet Explorer';
-	if (agt.indexOf("netscape") != -1) return 'Netscape';
-	if (agt.indexOf("mozilla/5.0") != -1) return 'Mozilla';
-	if (agt.indexOf('\/') != -1) {
-	if (agt.substr(0,agt.indexOf('\/')) != 'mozilla') {
-	return navigator.userAgent.substr(0,agt.indexOf('\/'));}
-	else return 'Netscape';} else if (agt.indexOf(' ') != -1)
-	return navigator.userAgent.substr(0,agt.indexOf(' '));
-	else return navigator.userAgent;
-}
+var key="AIzaSyCRfALTO27KAX-v5LrCm2bMzppRuimHBGA";
 
-function setOpacity(object, value)
-{
-	object.style.opacity = value/10;
-	object.style.filter = 'alpha(opacity=' + value*10 + ')';
-}
+var gplus=new function(){
+	this.init=function(){
 
-$(document).ready(function(){
-	
-	
-	
+	}
+	this.client_update=function(id,gid){
+		$.getJSON("https://www.googleapis.com/plus/v1/people/"+gid+"?callback=?&key="+key,function(data){
+			console.log(data);
+			//Vf daca e user data.isPlusUser
+			if(data.isPlusUser){
+				alert("Eroare, utilizatorul nu exista");			
+			}
+			/*Actualizare tabel ws*/
+			$("Cimage_"+id).html(data.image.url);
+			$("Cfname_"+id).html(data.name.familyName+" "+data.name.givenName);
+			
+			$.ajax({
+			  url: 'php/ajax.php?page=update&id='+id+'&gid='+gid+'&f='+data.name.familyName+'&n='+data.name.givenName+'&av='++data.image.url,
+			  success: function(data){
+			    $('.result').html(data);
+			    //data.id + " "+data.gender+" "+data.name.familyName+" "+data.name.givenName+" "+data.image.url
+			    
+			  }
+			});
+		});
+	}
+	this.client_sterge=function(id){
+		$.ajax({
+			url: 'php/ajax.php?page=delete&id='+id,
+			  success: function(data){
+			    $('.result').html(data);
+			    
+			  }
+		});
+	}
+	this.client_add=function(gid,pri){
+		$.getJSON("https://www.googleapis.com/plus/v1/people/"+gid+"?callback=?&key="+key,function(data){
+		if(data.isPlusUser){
+			alert("Eroare, utilizatorul nu exista");			
+		}
+		
+		$.ajax({
+		url: 'php/ajax.php?page=adaugare&gid='+id+'&g='+data.gender+'&f='+data.name.familyName+'&n='+data.name.givenName+'&av='+data.image.url+'&pri='+pri,
+			  success: function(data){
+			    $('.result').html(data);
+			    
+			  }
+		});
+	}
+}	
+
+
 });
