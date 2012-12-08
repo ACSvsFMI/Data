2008ef3c8d2d726ff056948e79e4287d7abc09ca
gamebak <alexandru.cobuz@gmail.com>
2012-12-08 17:02:16 +0200

    da

--- php/crawler.php
+++ php/crawler.php
@@ -1,12 +1,9 @@
 <?php 
-//Crawler g+
-//function sql_insert($table, $keys = array(), $values = array())
 
-/*
 define('ANTIHACK', TRUE);
 require_once('../config/_autoload.php');
 include_once('../libs/SQL_DB.php');
-*/
+
 
 $api_key="AIzaSyCRfALTO27KAX-v5LrCm2bMzppRuimHBGA";
 
@@ -20,37 +17,45 @@ function fetch($url) {
   curl_close($ch);
   return $data;
 }
+function init(){
+	$r=SQL_DB::sql_select(MYSQL_PRE."users",NULL,NULL,0,0,array('gid'));
+	foreach($r as $k=>$v){
+		crawl($v['gid']);
+	}
 
+}
 function crawl($gid){
 	global $api_key;
 	$sursa=fetch("https://www.googleapis.com/plus/v1/people/".$gid."/activities/public?maxResults=100&key=".$api_key);
 	$json=json_decode($sursa);
-	//Verificare Etag - daca luam sau nu datele
-		echo "Etag:".$json->{'etag'};
-	//echo "<pre>".var_dump($json->{'items'})."</pre>";
 	
 	for($i=0;$i<count($json->{'items'});$i++){
 	/*Content stire - plusoners - comentarii - shareuri - url_comm  - data postarii - titlu - Tip postare(share/post) - Etag Postare */
 		if(empty($json->{'items'}[0]->{'etag'})){
 			return "0002";
 		}
-		echo $json->{'items'}[$i]->{'object'}->{'content'}."<br/>"
-		.$json->{'items'}[$i]->{'object'}->{'plusoners'}->{'totalItems'}."<br/>"
-		.$json->{'items'}[$i]->{'object'}->{'replies'}->{'totalItems'}."<br/>"
-		.$json->{'items'}[$i]->{'object'}->{'resharers'}->{'totalItems'}."<br/>"
-		.$json->{'items'}[$i]->{'object'}->{'url'}."<br/>"
-		.date("H:i d/m/Y",strtotime($json->{'items'}[$i]->{'published'}))."<br/>"
-		.$json->{'items'}[$i]->{'title'}."<br/>"
-		.$json->{'items'}[$i]->{'verb'}."<br/>"
-		.$json->{'items'}[$i]->{'etag'}."<br/>";
+		if($r=SQL_DB::sql_count(MYSQL_PRE."posts","user_id='".$gid."' AND etag='".$json->{'items'}[0]->{'etag'}."'",'*','1')){
+			if($r)
+				return "Already Updated";
+
+		}
+		SQL_DB::sql_insert(MYSQL_PRE."posts", array('user_id'=>$gid,
+			'etag'=>$json->{'items'}[$i]->{'etag'},
+			'title'=>$json->{'items'}[$i]->{'title'},
+			'content'=>$json->{'items'}[$i]->{'object'}->{'content'},
+			'url_comm'=>$json->{'items'}[$i]->{'object'}->{'url'},
+			'share'=>$json->{'items'}[$i]->{'object'}->{'resharers'}->{'totalItems'},
+			'comms'=>$json->{'items'}[$i]->{'object'}->{'replies'}->{'totalItems'},
+			'plus'=>$json->{'items'}[$i]->{'object'}->{'plusoners'}->{'totalItems'},
+			'type'=>$json->{'items'}[$i]->{'verb'},
+			'datainsert'=>date("H:i d/m/Y",strtotime($json->{'items'}[$i]->{'published'}))
+		));
 
 	}
-	//SQL_DB::sql_insert(MYSQL_PRE."", array());
+
 	
 	unset($sursa,$json);
 }
 
-//111290814146404400956
-//107478352790883449995 -m
-crawl("111290814146404400956");
+init();
 ?>
