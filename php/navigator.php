dc337404f4cc38a5274b454fccbeadd6212d022b
Adrian Simionescu <adrypunctro@yahoo.com>
2012-12-08 21:56:02 +0200

    add client

--- php/navigator.php
+++ php/navigator.php
@@ -3,6 +3,5 @@
         	<ul class="menu">
                 <li><a href="index.php?page=posts">Management Postari</a></li>
                 <li><a href="index.php?page=users">Management Utilizatori</a></li>
-                <li><a href="index.php?page=setting">Set&#259;ri generale</a></li>
         	</ul>
         </nav><!-- end navigator -->
